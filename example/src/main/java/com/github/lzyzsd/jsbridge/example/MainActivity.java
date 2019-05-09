70e43296311c9307b77297fbb88c757ed5b49650
江辰 <jiangc@dtdream.com>
2019-05-09 17:12:10 +0800

    chore: add new test page

--- example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
+++ example/src/main/java/com/github/lzyzsd/jsbridge/example/MainActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.github.lzyzsd.jsbridge.BridgeHandler;
 import com.github.lzyzsd.jsbridge.BridgeWebView;
@@ -18,19 +19,11 @@ import com.github.lzyzsd.jsbridge.CallBackFunction;
 import com.github.lzyzsd.jsbridge.DefaultHandler;
 import com.google.gson.Gson;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity {
 
-	private final String TAG = "MainActivity";
+    private Toolbar toolbar;
 
-	BridgeWebView webView;
-
-	Button button;
-
-	int RESULT_CODE = 0;
-
-	ValueCallback<Uri> mUploadMessage;
-
-	ValueCallback<Uri[]> mUploadMessageArray;
+    private BridgeWebView webView;
 
     static class Location {
         String address;
@@ -39,117 +32,69 @@ public class MainActivity extends Activity implements OnClickListener {
     static class User {
         String name;
         Location location;
-        String testStr;
     }
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Button button = (Button) findViewById(R.id.button);
         webView = (BridgeWebView) findViewById(R.id.webView);
 
-		button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                webView.callHandler("tryRegister", "Button Clicked", new CallBackFunction() {
 
-		button.setOnClickListener(this);
+                    @Override
+                    public void onCallBack(String data) {
+                        Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
+                    }
 
-		webView.setDefaultHandler(new DefaultHandler());
+                });
+            }
+        });
 
-		webView.setWebChromeClient(new WebChromeClient() {
+        webView.setDefaultHandler(new DefaultHandler());
+        webView.setWebChromeClient(new WebChromeClient() {
 
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType, String capture) {
-				this.openFileChooser(uploadMsg);
-			}
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+                toolbar.setTitle(title);
+            }
+        });
+        webView.registerHandler("submitFromWeb", new BridgeHandler() {
 
-			@SuppressWarnings("unused")
-			public void openFileChooser(ValueCallback<Uri> uploadMsg, String AcceptType) {
-				this.openFileChooser(uploadMsg);
-			}
+            @Override
+            public void handler(String data, CallBackFunction function) {
+                function.onCallBack("Android: receive params " + data);
+            }
 
-			public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-				mUploadMessage = uploadMsg;
-				pickFile();
-			}
+        });
 
-			@Override
-			public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-				mUploadMessageArray = filePathCallback;
-				pickFile();
-				return true;
-			}
-		});
+        webView.loadUrl("file:///android_asset/test-site/index.html");
+        sendMessageToWeb();
+    }
 
-		webView.loadUrl("file:///android_asset/demo.html");
-
-		webView.registerHandler("submitFromWeb", new BridgeHandler() {
-
-			@Override
-			public void handler(String data, CallBackFunction function) {
-				Log.i(TAG, "handler = submitFromWeb, data from web = " + data);
-                function.onCallBack("submitFromWeb exe, response data 中文 from Java");
-			}
-
-		});
-
+    private void sendMessageToWeb() {
         User user = new User();
         Location location = new Location();
         location.address = "SDU";
         user.location = location;
         user.name = "大头鬼";
 
+        // js 端需注册一个 "functionInJs"
         webView.callHandler("functionInJs", new Gson().toJson(user), new CallBackFunction() {
             @Override
             public void onCallBack(String data) {
-
+                Toast.makeText(MainActivity.this, data, Toast.LENGTH_SHORT).show();
             }
         });
 
+        // js 端先调用 init
         webView.send("hello");
-
-	}
-
-	public void pickFile() {
-		Intent chooserIntent = new Intent(Intent.ACTION_GET_CONTENT);
-		chooserIntent.setType("image/*");
-		startActivityForResult(chooserIntent, RESULT_CODE);
-	}
-
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		if (requestCode == RESULT_CODE) {
-			if (null == mUploadMessage && null == mUploadMessageArray){
-				return;
-			}
-			if(null!= mUploadMessage && null == mUploadMessageArray){
-				Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-				mUploadMessage.onReceiveValue(result);
-				mUploadMessage = null;
-			}
-
-			if(null == mUploadMessage && null != mUploadMessageArray){
-				Uri result = intent == null || resultCode != RESULT_OK ? null : intent.getData();
-				mUploadMessageArray.onReceiveValue(new Uri[]{result});
-				mUploadMessageArray = null;
-			}
-
-		}
-	}
-
-	@Override
-	public void onClick(View v) {
-		if (button.equals(v)) {
-            webView.callHandler("functionInJs", "data from Java", new CallBackFunction() {
-
-				@Override
-				public void onCallBack(String data) {
-					// TODO Auto-generated method stub
-					Log.i(TAG, "reponse data from js " + data);
-				}
-
-			});
-		}
-
-	}
-
+    }
 }
